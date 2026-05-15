45bbcb8a8ec40e97f3af8a0807c14f0ecfa93583
janani-s853 <sjaanu2005@gmail.com>
2026-05-16 02:20:13 +0530

    Update Supabase configuration with placeholders
    Replaced hardcoded Supabase URL and key with placeholders.

--- constants/supabase.ts
+++ constants/supabase.ts
@@ -1,7 +1,7 @@
 import 'react-native-url-polyfill/auto';  // ⭐ VERY IMPORTANT
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://lamofoaiznmraetwgbfw.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxhbW9mb2Fpem5tcmFldHdnYmZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY4MjA5MTQsImV4cCI6MjA4MjM5NjkxNH0.cZSvm-PajM97AlmmmxQtkk5WntXHeIUoGiT5PU6awWk";
+SUPABASE_URL = "your_supabase_url"
+SUPABASE_KEY = "your_supabase_key"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
